419f5af34c69a11f03d1b67d5ec732cd626d3d88
Steven Speek <slspeek@gmail.com>
2011-06-24 12:26:20 +0200

    Idempotence

--- install.sh
+++ install.sh
@@ -1,5 +1,14 @@
 cd ~
 sudo apt-get install curl sun-java6-jdk python2.6-dev build-essential imagemagick sqlitebrowser mercurial
-wget http://googleappengine.googlecode.com/files/appengine-java-sdk-1.5.0.zip && unzip appengine-java-sdk-1.5.0.zip
-curl http://apache.osuosl.org//maven/binaries/apache-maven-3.0.3-bin.tar.gz|tar -C ~ -xvzf -
-hg clone https://java.fspotcloud.googlecode.com/hg/ fspotcloud
+if [ ! -d ~/appengine-java-sdk-1.5.0 ]; then
+	wget http://googleappengine.googlecode.com/files/appengine-java-sdk-1.5.0.zip && unzip appengine-java-sdk-1.5.0.zip
+fi
+if [ ! -d ~/apache-maven-3.0.3 ]; then
+	url http://apache.osuosl.org//maven/binaries/apache-maven-3.0.3-bin.tar.gz|tar -C ~ -xvzf -
+fi
+if [ ! -d ~/fspotcloud ]; then
+	hg clone https://java.fspotcloud.googlecode.com/hg/ fspotcloud\
+fi
+cd ~/fspotcloud
+export PATH=~/apache-maven-3.0.3/bin
+mvn
