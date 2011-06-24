3c389cd31b5bbef2f3089c5cd43a52b313f6b4a3
Steven Speek <slspeek@gmail.com>
2011-06-24 12:02:34 +0200

    Installation script setup

---
+++ install.sh
@@ -0,0 +1,5 @@
+cd ~
+sudo apt-get install curl sun-java6-jdk python2.6-dev build-essential imagemagick sqlitebrowser mercurial
+wget http://googleappengine.googlecode.com/files/appengine-java-sdk-1.5.0.zip && unzip appengine-java-sdk-1.5.0.zip
+curl http://apache.osuosl.org//maven/binaries/apache-maven-3.0.3-bin.tar.gz|tar -C ~ -xvzf -
+hg clone https://java.fspotcloud.googlecode.com/hg/ fspotcloud
