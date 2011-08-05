93e7c9b8b272b42405716de462d223ffd98550f5
slspeek <slspeek@gmail.com>
2011-08-05 09:44:56 +0000

    sun java alternative set

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cd ~
 sudo apt-get update && sudo apt-get install --yes --force-yes curl sun-java6-jdk python2.6-dev build-essential imagemagick sqlitebrowser mercurial iceweasel
+update-alternatives --set java  /usr/lib/jvm/java-6-sun/jre/bin/java 
 if [ ! -d ~/appengine-java-sdk-1.5.0 ]; then
 	wget http://googleappengine.googlecode.com/files/appengine-java-sdk-1.5.0.zip && unzip appengine-java-sdk-1.5.0.zip
 fi
