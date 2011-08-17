e8a289cdd67e2705c8db0b2eebb9643e513d3c52
slspeek <slspeek@gmail.com>
2011-08-17 12:15:14 +0000

    $HOME instead of ~

--- env.sh
+++ env.sh
@@ -8,6 +8,6 @@ alias buildpeer='cd peer && mvn clean install && cd war && mvn gae:stop clean ve
 alias cleanrun='cd war && stop ; mvn clean gae:run'
 alias deploy='cd war/ && mvn -Dgae.deps.split -Dbot.secret=$YOUR_SECRET gae:deploy'
 alias localpeer='cd peer/ && java -Dendpoint=localhost:8080 -Dbot.secret=$YOUR_SECRET -Ddb=~/fspotcloud/peer/src/test/resources/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
-alias peer='cd ~/fspotcloud/peer/ && java -Djava.util.logging.config.file=target/classes/logging.properties -Dendpoint=$YOUR_APPENGINE_DEPLOYMENT -Dbot.secret=$YOUR_SECRET -Ddb=~/.config/f-spot/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
+alias peer='cd ~/fspotcloud/peer/ && java -Djava.util.logging.config.file=target/classes/logging.properties -Dendpoint=$YOUR_APPENGINE_DEPLOYMENT -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/.config/f-spot/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
 alias runlocal='cd ~/fspotcloud && cd war && mvn gae:stop gae:run'
 alias stopall='(cd ~/fspotcloud/war && mvn gae:stop) && telnet localhost 4444'
