4710602fc4c756b1a5cb3fab1a2b17b65b7376df
Steven Speek <slspeek@gmail.com>
2011-09-14 12:17:54 +0200

    Add sqlbrowser alias

--- env.sh
+++ env.sh
@@ -10,13 +10,14 @@ alias stopall='(cd war && mvn gae:stop); telnet localhost 4444'
 alias build='stopall; cd ~/fspotcloud && time (mvn clean ; mvn)'
 alias buildserver='cd server && mvn clean install && cd war && mvn gae:stop clean verify'
 alias buildpeer='cd peer && mvn clean install && cd war && mvn gae:stop clean verify'
-alias cleanrun='cd war && stop ; mvn clean gae:run'
+alias cleanrun='cd war && stopall ; mvn clean gae:run'
 alias deploy='cd war/ && mvn -Dgae.deps.split -Dbot.secret=$YOUR_SECRET gae:deploy'
 alias localpeer='cd peer/ && java -Dendpoint=localhost:8080 -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/fspotcloud/peer/src/test/resources/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
 alias peer='cd ~/fspotcloud/peer/ && java -Djava.util.logging.config.file=target/classes/logging.properties -Dendpoint=$YOUR_APPENGINE_DEPLOYMENT -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/.config/f-spot/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
 alias runlocal='cd ~/fspotcloud && cd war && mvn gae:stop gae:run'
 alias verify='(stopall; cd war; mvn clean verify -Dnodelete)'
 alias gwt='(cd client &&  mvn gwt:run)'
+alias viewtestdb='sqlitebrowser $HOME/fspotcloud/peer/src/test/resources/photos.db'
 
 function resume() {
    mvn install -rf :$1;
