f9a3afcd17f29c2f606a32dc02631d79b41c8fb0
Steven Speek <slspeek@gmail.com>
2011-09-14 11:27:20 +0200

    build_f-spot.sh more robust (fails on error).

--- build_f-spot.sh
+++ build_f-spot.sh
@@ -1,3 +1,4 @@
+set -e
 apt-get update
 apt-get install apt-src
 apt-src update
@@ -6,4 +7,4 @@ apt-src install f-spot
 wget http://ftp.gnome.org/pub/GNOME/sources/f-spot/0.8/f-spot-0.8.2.tar.bz2
 tar xvjf f-spot-0.8.2.tar.bz2
 cd f-spot-0.8.2
-./congigure && make && make install
+./configure && make && make install
