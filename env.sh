cc7276d68f76495292d97176746b2b9b49ad224f
Steven L. Speek <slspeek@gmail.com>
2011-09-14 21:06:40 +0200

    One more alias and localpeer faster and production peer slower.

--- env.sh
+++ env.sh
@@ -12,12 +12,13 @@ alias buildserver='cd server && mvn clean install && cd war && mvn gae:stop clea
 alias buildpeer='cd peer && mvn clean install && cd war && mvn gae:stop clean verify'
 alias cleanrun='cd war && stopall ; mvn clean gae:run'
 alias deploy='cd war/ && mvn -Dgae.deps.split -Dbot.secret=$YOUR_SECRET gae:deploy'
-alias localpeer='cd peer/ && java -Dendpoint=localhost:8080 -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/fspotcloud/peer/src/test/resources/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
-alias peer='cd ~/fspotcloud/peer/ && java -Djava.util.logging.config.file=target/classes/logging.properties -Dendpoint=$YOUR_APPENGINE_DEPLOYMENT -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/.config/f-spot/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
+alias localpeer='cd peer/ && java -Dpause=3000 -Dendpoint=localhost:8080 -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/fspotcloud/peer/src/test/resources/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
+alias peer='cd ~/fspotcloud/peer/ && java -Dpause=30000 -Djava.util.logging.config.file=target/classes/logging.properties -Dendpoint=$YOUR_APPENGINE_DEPLOYMENT -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/.config/f-spot/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
 alias runlocal='cd ~/fspotcloud && cd war && mvn gae:stop gae:run'
 alias verify='(stopall; cd war; mvn clean verify -Dnodelete)'
 alias gwt='(cd client &&  mvn gwt:run)'
 alias viewtestdb='sqlitebrowser $HOME/fspotcloud/peer/src/test/resources/photos.db'
+alias reclipse='cd ~/fspotcloud && mvn eclipse:clean google:clean eclipse:eclipse google:eclipse'
 
 function resume() {
    mvn install -rf :$1;
