76e01c6727a07ac7fe9d6410d6fea8ffa64285bb
Steven Speek <slspeek@gmail.com>
2011-10-17 14:19:18 +0200

    update alternatives via sudo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 GAE_VERSION=1.5.3
 cd ~
 sudo apt-get update && sudo apt-get install --yes --force-yes curl sun-java6-jdk imagemagick sqlitebrowser mercurial iceweasel graphviz exif
-update-alternatives --set java  /usr/lib/jvm/java-6-sun/jre/bin/java 
+sudo update-alternatives --set java  /usr/lib/jvm/java-6-sun/jre/bin/java 
 if [ ! -d ~/appengine-java-sdk-$GAE_VERSION ]; then
 	wget http://googleappengine.googlecode.com/files/appengine-java-sdk-$GAE_VERSION.zip && unzip appengine-java-sdk-$GAE_VERSION.zip
 fi
