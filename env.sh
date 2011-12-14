c37bba2582a4ecfcc6016abbebe6ee14d7698403
Steven L. Speek <slspeek@gmail.com>
2011-12-14 22:20:14 +0100

    b for individual builds.

--- env.sh
+++ env.sh
@@ -15,9 +15,10 @@ alias buildserver='cd server && mvn clean install && cd war-prod && mvn gae:stop
 alias buildpeer='cd peer && mvn clean install && cd war && mvn gae:stop clean verify'
 alias cleanrun='cd war-prod && stopall ; mvn clean gae:run'
 alias deploy='cd war-prod/ && mvn -Dgae.deps.split -Dbot.secret=$YOUR_SECRET gae:deploy'
-alias localpeer='cd peer/ && java -Dpause=3000 -Dendpoint=localhost:8080 -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/fspotcloud/peer/src/test/resources/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
+alias localpeer='cd peer/ && java -Dphoto.dir.override=file:/$HOME/fspotcloud/peer/src/test/resources/Photos -Dphoto.dir.original=file:///home/steven/Photos -Dpause=3000 -Dendpoint=localhost:8080 -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/fspotcloud/peer/src/test/resources/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
 alias localbot='cd bot-dispatch/test-bot/ && java -Dpause=3000 -Dendpoint=localhost:8080 -Dbot.secret=$YOUR_SECRET -jar target/test-bot-*-jar-with-dependencies.jar '
 alias peer='cd ~/fspotcloud/peer/ && java -Dpause=30000 -Djava.util.logging.config.file=target/classes/logging.properties -Dendpoint=$YOUR_APPENGINE_DEPLOYMENT -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/.config/f-spot/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
+alias peerprodlocal='cd ~/fspotcloud/peer/ && java -Dpause=5000 -Djava.util.logging.config.file=target/classes/logging.properties -Dendpoint=localhost:8080 -Dbot.secret=$YOUR_SECRET -Ddb=$HOME/.config/f-spot/photos.db  -jar target/peer-*-jar-with-dependencies.jar '
 alias runlocal='cd ~/fspotcloud && cd war && mvn gae:stop gae:run'
 alias verify='(stopall; cd war; mvn clean verify -Dnodelete)'
 alias gwt='(cd client &&  mvn gwt:run)'
@@ -29,8 +30,14 @@ alias rununinstaller='java -jar ~/FSpotCloud/Uninstaller/uninstaller.jar'
 function clean() {
   (cd $1 && mvn clean);
 }
+function sb() {
+  (cd $1 && mvn clean ; rm -rf target; mvn install -Dmaven.test.error.ignore -Dmaven.test.failure.ignore)
+}
+function b() {
+  (cd $1 && mvn clean ; rm -rf target; mvn install);
+}
 alias cheapclean='clean war; clean server; clean peer; clean model-api; clean model;'
-
+alias cheapbuild='b rpc && b peer-rpc && b peer && b model && b server && b war'
 function resume() {
    mvn install -rf :$1;
 }
