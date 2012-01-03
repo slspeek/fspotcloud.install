7bff43693c1cfcf1f8333910b9a0292c85d7f454
Steven L. Speek <slspeek@gmail.com>
2012-01-03 22:44:10 +0100

    Added gradle to the tools.

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+GAE_VERSION=1.6.0
+cd ~
+if [ ! -d gradle-1.0-milestone-6 ]; then
+  wget http://repo.gradle.org/gradle/distributions/gradle-1.0-milestone-6-all.zip && unzip gradle-1.0-milestone-6-all.zip
+fi
+sudo apt-get update && sudo apt-get install --yes --force-yes curl sun-java6-jdk imagemagick sqlitebrowser mercurial iceweasel graphviz exif
+sudo update-alternatives --set java  /usr/lib/jvm/java-6-sun/jre/bin/java 
+if [ ! -d ~/appengine-java-sdk-$GAE_VERSION ]; then
+	wget http://googleappengine.googlecode.com/files/appengine-java-sdk-$GAE_VERSION.zip && unzip appengine-java-sdk-$GAE_VERSION.zip
+fi
+if [ ! -d ~/apache-maven-3.0.3 ]; then
+	curl http://apache.osuosl.org//maven/binaries/apache-maven-3.0.3-bin.tar.gz|tar -C ~ -xvzf -
+fi
+if [ ! -d ~/fspotcloud ]; then
+	hg clone https://java.fspotcloud.googlecode.com/hg/ fspotcloud
+fi
+cd fspotcloud.install
+source ./env.sh
+cd ~/fspotcloud
+hg pull -u
+mvn
