4f938366192a86c317d8799742094ca830cfba0f
Steven L. Speek <slspeek@gmail.com>
2012-03-01 18:51:08 +0100

    jenkins spelling errors corrected

--- jenkins.sh
+++ jenkins.sh
@@ -1,4 +1,4 @@
 wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
 echo deb http://pkg.jenkins-ci.org/debian binary/>>/etc/apt/sources.list
-sudo apt-get --yess --force-yess update
-sudo apt-get --yes --force-yess install jenkins
+sudo apt-get --yes --force-yes update
+sudo apt-get --yes --force-yes install jenkins
