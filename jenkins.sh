b55f79154c174181fd561737d866eb75bf60358e
Steven L. Speek <slspeek@gmail.com>
2012-03-01 19:06:56 +0100

    defaults for jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -2,6 +2,8 @@ wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key 
 echo deb http://pkg.jenkins-ci.org/debian binary/>>/etc/apt/sources.list
 sudo apt-get --yes --force-yes update
 sudo apt-get --yes --force-yes install jenkins
-sed -i -e 's^/var/lib/jenkins^/home/jenkins^' /etc/passwd
+sed -i -e 's^/var/lib/jenkins^/home/jenkins^' /etc/passwd /etc/default/jenkins
 mkdir /home/jenkins
 chown -R jenkins /home/jenkins
+sed -i -e 's/8080/9000/' /etc/default/jenkins
+/etc/init.d/jenkins restart
