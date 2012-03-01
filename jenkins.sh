107f12e8b469d10595b7523a6f406c684559dbce
Steven L. Speek <slspeek@gmail.com>
2012-03-01 18:55:29 +0100

    jenkins home dir

--- jenkins.sh
+++ jenkins.sh
@@ -2,3 +2,6 @@ wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key 
 echo deb http://pkg.jenkins-ci.org/debian binary/>>/etc/apt/sources.list
 sudo apt-get --yes --force-yes update
 sudo apt-get --yes --force-yes install jenkins
+sed -i -e 's^/var/lib/jenkins^/home/jenkins^' /etc/passwd
+mkdir /home/jenkins
+chown -R jenkins /home/jenkins
