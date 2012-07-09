c9c826cf43ada28b741158749c937b53e5e47745
Steven L. Speek <slspeek@gmail.com>
2012-07-09 23:43:06 +0200

    updated to allmost all gradle 1.0

--- deploy-gae-lc-rt-file.sh
+++ deploy-gae-lc-rt-file.sh
@@ -1,1 +1,2 @@
-mvn deploy:deploy-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=1.7.0 -Dpackaging=jar -Dfile=$HOME/appengine-java-sdk-1.7.0/lib/impl/appengine-local-runtime.jar -Durl=dav:https://fspotcloud-maven.googlecode.com/svn/maven/repository -DrepositoryId=fspotcloud-maven
+GAE_VERSION=1.6.6
+mvn deploy:deploy-file -DgroupId=com.google.appengine -DartifactId=appengine-local-runtime -Dversion=$GAE_VERSION -Dpackaging=jar -Dfile=$HOME/appengine-java-sdk-$GAE_VERSION/lib/impl/appengine-local-runtime.jar -Durl=dav:https://fspotcloud-maven.googlecode.com/svn/maven/repository -DrepositoryId=fspotcloud-maven
