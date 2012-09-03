77024c17a9b2bf3fbf61b9d429dabd85b7a65181
Steven L. Speek <slspeek@gmail.com>
2012-09-03 20:07:52 +0200

    separated gae-sdk install

---
+++ install-gae-sdk.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+GAE_VERSION=1.7.0
+cd ~
+if [ ! -d ~/appengine-java-sdk-$GAE_VERSION ]; then
+	wget http://googleappengine.googlecode.com/files/appengine-java-sdk-$GAE_VERSION.zip && unzip appengine-java-sdk-$GAE_VERSION.zip
+fi
