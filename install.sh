f36f647a89f86f4595df0c450514147169023001
Steven L. Speek <slspeek@gmail.com>
2013-06-24 22:45:34 +0200

    new maven version

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ if [ ! -d gradle-1.1 ]; then
   wget http://services.gradle.org/distributions/gradle-1.1-all.zip  && unzip gradle-1.1-all.zip
 fi
 if [ ! -d ~/apache-maven-3.0.4 ]; then
-	curl http://apache.osuosl.org//maven/binaries/apache-maven-3.0.4-bin.tar.gz|tar -C ~ -xvzf -
+	curl http://apache.osuosl.org//maven/binaries/apache-maven-3.1.0-alpha-1-bin.tar.gz|tar -C ~ -xvzf -
 fi
