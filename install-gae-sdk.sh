b281bbe37eb2f795bb16a8cf98401c68631886c5
Steven L. Speek <slspeek@gmail.com>
2016-04-14 21:04:15 +0200

    GAE 1.9.34

--- install-gae-sdk.sh
+++ install-gae-sdk.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-GAE_VERSION=1.9.18
+GAE_VERSION=1.9.34
 cd ~
 if [ ! -d ~/appengine-java-sdk-$GAE_VERSION ]; then
 	wget https://storage.googleapis.com/appengine-sdks/featured/appengine-java-sdk-$GAE_VERSION.zip && unzip appengine-java-sdk-$GAE_VERSION.zip
